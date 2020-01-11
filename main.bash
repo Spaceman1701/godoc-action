5a9b99a70dea7d0eead36a786a591de196855f74
Ethan Hunter <fc.spaceman@gmail.com>
2020-01-10 18:40:15 -0700

    maybe make it work with gh-pages

--- main.bash
+++ main.bash
@@ -26,8 +26,9 @@ rm -rf doc lib "$PR_NUMBER" pkg # Delete previous documents.
 mv localhost:8080/* .
 echo "moved doc to root"
 rm -rf localhost:8080
-find pkg -type f -exec sed -i  "s#/lib/godoc#/$REPO_NAME/lib/godoc#g" {} +
-mv "pkg/$MODULE_ROOT/index.html" .
+find pkg -type f -exec sed -i  "s#/lib/godoc#/$MODULE_ROOT/lib/godoc#g" {} +
+mv "pkg/$MODULE_ROOT/index.html" index.html
+echo "configured for gh-pages"
 git config --local user.email "action@github.com"
 git config --local user.name "GitHub Action"
 #[ -d "$PR_NUMBER" ] || mkdir "$PR_NUMBER"
